bdba9bf21821739ade3d8d71909496a63f8c1b9a
Jordan Xion <xion4762@gmail.com>
2022-03-26 00:23:47 -0400

    Navigate between pages

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<div className="container">
 			<Head>
-				<title>Create Next App</title>
+				<title>Jordan's Next.js Blog</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main>
 				<h1 className="title">
-					Learn <a href="https://nextjs.org">Next.js!</a>
+					Read{" "}
+					<Link href="/posts/first-post">
+						<a>this page!</a>
+					</Link>
 				</h1>
 
 				<p className="description">
@@ -20,12 +24,18 @@ export default function Home() {
 				<div className="grid">
 					<a href="https://nextjs.org/docs" className="card">
 						<h3>Documentation &rarr;</h3>
-						<p>Find in-depth information about Next.js features and API.</p>
+						<p>
+							Find in-depth information about Next.js features and
+							API.
+						</p>
 					</a>
 
 					<a href="https://nextjs.org/learn" className="card">
 						<h3>Learn &rarr;</h3>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
+						<p>
+							Learn about Next.js in an interactive course with
+							quizzes!
+						</p>
 					</a>
 
 					<a
@@ -33,7 +43,10 @@ export default function Home() {
 						className="card"
 					>
 						<h3>Examples &rarr;</h3>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
+						<p>
+							Discover and deploy boilerplate example Next.js
+							projects.
+						</p>
 					</a>
 
 					<a
@@ -42,7 +55,8 @@ export default function Home() {
 					>
 						<h3>Deploy &rarr;</h3>
 						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
+							Instantly deploy your Next.js site to a public URL
+							with Vercel.
 						</p>
 					</a>
 				</div>
@@ -54,7 +68,8 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
+					Powered by{" "}
+					<img src="/vercel.svg" alt="Vercel" className="logo" />
 				</a>
 			</footer>
 
@@ -134,7 +149,8 @@ export default function Home() {
 					padding: 0.75rem;
 					font-size: 1.1rem;
 					font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-						DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+						DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
+						monospace;
 				}
 
 				.grid {
@@ -194,9 +210,9 @@ export default function Home() {
 				body {
 					padding: 0;
 					margin: 0;
-					font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-						Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-						sans-serif;
+					font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
+						Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+						Helvetica Neue, sans-serif;
 				}
 
 				* {
